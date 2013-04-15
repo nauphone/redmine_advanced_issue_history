fea85f9ab216489513807fb343c43368bf9a46e5
Evgeny Tataurov <etataurov@naumen.ru>
2013-04-15 14:28:44 +0600

    mailing on new watcher should work

--- lib/redmine_advanced_issue_history/patches/mailer_patch.rb
+++ lib/redmine_advanced_issue_history/patches/mailer_patch.rb
@@ -23,15 +23,15 @@ module RedmineAdvancedIssueHistory
           message_id journal
           references issue
           @author = journal.user
-          recipients [watcher]
+          recipients = [watcher]
           s = "[#{issue.project.name} - #{issue.tracker.name} ##{issue.id}] "
           s << "(#{issue.status.name}) " if journal.new_value_for('status_id')
           s << issue.subject
-          subject s
-          body :issue => issue,
-               :journal => journal,
-               :issue_url => url_for(:controller => 'issues', :action => 'show', :id => issue, :anchor => "change-#{journal.id}")
-          render_multipart('issue_edit', body)
+	  @issue = issue
+	  @journal = journal
+	  @issue_url = url_for(:controller => 'issues', :action => 'show', :id => issue, :anchor => "change-#{journal.id}")
+	  mail :to => recipients,
+	       :subject => s
         end
       end
     end
