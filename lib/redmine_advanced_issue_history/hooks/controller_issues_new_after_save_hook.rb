ff8d2fd53b70e69745bfee9ac3d581e86a8d7b85
Alexei Margasov <amargasov@naumen.ru>
2014-09-03 18:02:39 +0600

    Task #32525 Уведомление при закрытии всех подзадач мета-тикета

--- lib/redmine_advanced_issue_history/hooks/controller_issues_new_after_save_hook.rb
+++ lib/redmine_advanced_issue_history/hooks/controller_issues_new_after_save_hook.rb
@@ -30,19 +30,39 @@ module RedmineAdvancedIssueHistory
             end
           end
         end
+        unless issue.root == issue
+          root_issue = issue.root
+          open_issues = root_issue.descendants.reject {|x| x if x.closed? or x == issue}
+          unless IssueStatus.find(issue_params[:status_id].to_i).is_closed?
+            open_issues.append(issue)
+          end
+          unless open_issues.any?
+            root_task_note = "All sub task was closed"
+            root_task_journal = Journal.new(:journalized => root_issue, :user => User.current, :notes => root_task_note, :is_system_note=> true)
+            root_task_journal.save
+          end
+        end
       end
 
       def controller_issues_new_after_save(context={})
         issue = context[:issue]
         unless issue.parent.nil?
           parent_issue = issue.parent
-          user = User.current
           parent_task_note = "Sub task '#{issue}' was added"
           sub_task_note = "Has been added as sub task to '#{parent_issue}'"
 
-          parent_task_journal  = Journal.new(:journalized => parent_issue, :user => user, :notes => parent_task_note, :is_system_note=> true)
-          sub_task_journal = Journal.new(:journalized => issue, :user => user, :notes => sub_task_note, :is_system_note=> true)
+          parent_task_journal  = Journal.new(:journalized => parent_issue, :user => User.current, :notes => parent_task_note, :is_system_note=> true)
+          sub_task_journal = Journal.new(:journalized => issue, :user => User.current, :notes => sub_task_note, :is_system_note=> true)
           parent_task_journal.save && sub_task_journal.save
+        end
+        unless issue.root == issue
+          root_issue = issue.root
+          open_issues = root_issue.descendants.reject {|x| x if x.closed? }
+          unless open_issues.any?
+            root_task_note = "All sub task was closed"
+            root_task_journal = Journal.new(:journalized => root_issue, :user => User.current, :notes => root_task_note, :is_system_note=> true)
+            root_task_journal.save
+          end
         end
       end
     end
